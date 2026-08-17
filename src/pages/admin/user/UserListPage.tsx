f283e0def414fd57997774dbe4bc4ef3cf856d0d
ZoFitahiana <hei.tsiaroniaina@gmail.com>
2026-08-17 10:15:56 +0200

    feat:Update main branch

--- src/pages/admin/user/UserListPage.tsx
+++ src/pages/admin/user/UserListPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Pencil, Trash, Plus } from "lucide-react";
+import { Eye, Pencil, Trash, Plus } from "lucide-react";
 
 import GlobalTable from "@/components/table/GlobalTable";
 import TableHeader from "@/components/table/TableHeader";
